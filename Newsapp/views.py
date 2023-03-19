68e7347ea526f7005e40e60dfd59d4636ec5b436
Ayush Singh <78557460+Ayush-singh-07@users.noreply.github.com>
2023-03-19 17:24:59 +0530

    Update views.py

--- Newsapp/views.py
+++ Newsapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
-API_KEY='ebf5dfbf8a0746c981fda70acb7ecc25'
+API_KEY=''+process.env.API_KEY
 
 
 # Create your views here.
